dcbf12134a44f96e2876806e92e9d6ce604abaa4
imadkaj <54841665+imadkaj@users.noreply.github.com>
2019-12-05 12:20:17 +0100

    willekeurig
    php file

---
+++ willekeurig.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="stylecssgalgje.css">
+  </head>
+  <body>
+<h1>Je hebt gekozen voor een willekeurig woord</h1>
+<?php
+    $woorden = array('Imad', 'ROCAmsterdam' , "Thijs" , "Dehaas28" , "Sinterklaas", "Kerstvakantie", "Bob", "Thomas" );
+    $rand_keys = array_rand($woorden);
+    setcookie('woord' , $woorden[$rand_keys] , time() + (86400 * 10) );
+    echo $woorden[$rand_keys];
+    ?>
+    <form action="game.php" method="get">
+      <button type="submit" name="button">Begin het spel</button>
+    </form>
+  </body>
+</html>
